db353c02f717076524bd1d322f634569e58bfd3b
dkstm95 <dkstm95@naver.com>
2024-04-28 11:42:02 +0900

    feat: add common(annotation, response, etc)

---
+++ src/main/java/com/seungilahn/common/PersistenceAdapter.java
@@ -0,0 +1,22 @@
+package com.seungilahn.common;
+
+import org.springframework.core.annotation.AliasFor;
+import org.springframework.stereotype.Component;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Component
+public @interface PersistenceAdapter {
+
+  /**
+   * The value may indicate a suggestion for a logical component nickname,
+   * to be turned into a Spring bean in case of an autodetected component.
+   * @return the suggested component nickname, if any (or empty String otherwise)
+   */
+  @AliasFor(annotation = Component.class)
+  String value() default "";
+
+}
